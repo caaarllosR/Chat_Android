6dc4bebd7340510b949b3ae6ea5bb6380ecd859c
caaarllosr <carlosrvmelo@gmail.com>
2020-08-05 11:06:58 -0300

    first commit

---
+++ chat/lib/main.dart
@@ -0,0 +1,25 @@
+import 'package:chat/chart_screen.dart';
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+
+void main() {
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Chat flutter',
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+        iconTheme: IconThemeData(
+          color: Colors.blue
+        )
+      ),
+      home: ChatScreen(),
+    );
+  }
+}
